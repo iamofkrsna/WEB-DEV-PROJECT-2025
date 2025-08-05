ca2495879b8da277ff46b8afc04f7742c12319a7
iamofkrsna <hariom.vedas@gmail.com>
2025-08-05 14:39:57 +0530

    Added Warning System

--- TIC-TAC-TOE-PROJECT/t-t-t-Home.js
+++ TIC-TAC-TOE-PROJECT/t-t-t-Home.js
@@ -1,12 +1,31 @@
 let nextBtnX = document.querySelector(".player-x-next");
 let mainContent = document.querySelector(".main-content");
 let loadingAnimation = document.querySelector(".loading-full-screen");
+let playerXName = document.querySelector(".player-x-input");
+let warning = document.querySelector(".warning");
 
-console.dir(loadingAnimation);
-
-nextBtnX.addEventListener("click", () => {
-  loadingAnimation.style.display = "flex";
+//On Name X TextBox Clicked
+playerXName.addEventListener("click", () => {
+  //Hiddening the warning if Visible
   setTimeout(() => {
-    loadingAnimation.style.display = "none";
-  }, Math.random() * (3000 - 700) + 700);
+    warning.style.display = "none";
+  }, Math.random() * (200 - 50) + 50);
+});
+
+//On NEXT button Clicked
+let nameX = playerXName.value.trim();
+nextBtnX.addEventListener("click", () => {
+  //Checking if the Text Box is blank
+  nameX = playerXName.value.trim();
+  if (nameX === "") {
+    warning.innerText = "Please Enter Player X Name.";
+    warning.style.display = "flex";
+  } else {
+    loadingAnimation.style.display = "flex";
+    setTimeout(() => {
+      loadingAnimation.style.display = "none";
+      //Loading the Player-O Window
+      /*WORK HERE *******************<<<<<<<<<<<<<<<<=============*/
+    }, Math.random() * (3000 - 700) + 700);
+  }
 });
